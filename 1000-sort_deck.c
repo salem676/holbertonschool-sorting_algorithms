2be913b604922485d4ba90bba96201a81ec89c6e
salem676 <echoesofeternal@gmail.com>
2022-07-22 16:14:53 -0500

    try

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,9 +1,8 @@
 #include "deck.h"
 /**
- * aux_num_fun - turn into integer card value.
- * Description: as above.
- * @head_tmp1: pointer to the list.
- * Return: integer rep.
+ * aux_num_fun - turn into integer card value
+ * @head_tmp1: pointer to the list
+ * Return: integer rep
  **/
 int aux_num_fun(deck_node_t *head_tmp1)
 {
@@ -11,36 +10,26 @@ int aux_num_fun(deck_node_t *head_tmp1)
 	int num[13] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 	char val[13] = {'A', '2', '3', '4', '5', '6', '7',
 		'8', '9', '1', 'J', 'Q', 'K'};
-	/*
-	 * @aux_num: dummy.
-	 * @j: counter.
-	 * @num: array numbers.
-	 * @val: array with symbols.
-	 */
+
 	for (j = 0; j < 13; j++)
 	{
 		if (head_tmp1->card->value[0] == val[j])
 			aux_num = num[j];
 	}
+
 	return (aux_num);
 }
 /**
- * num_sort - sorts a doubly linked list of integers, 4 stages.
- * Description: as above.
- * @list: pointer to the list head.
- * Return: no return.
+ * num_sort - sorts a doubly linked list of integers, 4 stages
+ * @list: pointer to the list head
+ * Return: no return
  **/
 void num_sort(deck_node_t **list)
 {
 	deck_node_t *head_tmp1, *head_tmp2, *aux1, *aux2;
 	int flag = 0, i, aux_num1, aux_num2;
 	unsigned int k;
-	/*
-	 * @head_tmp1: ptr to type, @head_tmp2: ptr to type.
-	 * @aux1: ptr to type, @aux2: ptr to type.
-	 * @flag: in zero, @i: counter, @aux_num1: aux, @aux_num2: aux.
-	 * @k: counter.
-	 */
+
 	head_tmp1 = *list;
 	head_tmp2 = *list;
 	for (i = 0; i < 4; i++)
@@ -80,20 +69,15 @@ void num_sort(deck_node_t **list)
 }
 /**
  * kind_sort - sorts a doubly linked list of integers
- * in ascending order using the Insertion sort ailgorithm.
- * Description: as above.
- * @list: pointer to the list head.
- * Return: no return.
+ * in ascending order using the Insertion sort ailgorithm
+ * @list: pointer to the list head
+ * Return: no return
  **/
 void kind_sort(deck_node_t **list)
 {
 	deck_node_t *head_tmp1, *head_tmp2, *aux1, *aux2;
 	int flag;
-	/*
-	 * @head_tmp1: ptr to type, @head_tmp2: ptr to type.
-	 * @aux1: ptr to type, @aux2: ptr to type.
-	 * @flag: int flag.
-	 */
+
 	if (list)
 	{
 		head_tmp1 = *list;
@@ -132,10 +116,10 @@ void kind_sort(deck_node_t **list)
 	}
 }
 /**
- * sort_deck - sorts a deck of cards.
- * Description: as above.
- * @deck: pter to the deck.
- * Return: no return.
+ * sort_deck - sorts a deck of cards
+ * @deck: ponter to the deck
+ * Return: no return
+ *
  **/
 void sort_deck(deck_node_t **deck)
 {
